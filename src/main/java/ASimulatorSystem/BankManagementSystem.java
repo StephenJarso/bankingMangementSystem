825c751a249cffe7d11c2bc57a8e1e7e13c5a3ee
sjarso <stephenjacob815@gmail.com>
2026-05-15 05:56:28 +0300

    Initial setup: Create project structure and basic main class

---
+++ src/main/java/ASimulatorSystem/BankManagementSystem.java
@@ -0,0 +1,12 @@
+package ASimulatorSystem;
+
+/**
+ *
+ * @author sjarso
+ */
+public class BankManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
